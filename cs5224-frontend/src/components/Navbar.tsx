09c367cd28dada4fda2c1e7d53424b56bbcf682c
Thang Pham <bathang02@gmail.com>
2024-04-21 14:18:53 +0800

    sticky navbar

--- cs5224-frontend/src/components/Navbar.tsx
+++ cs5224-frontend/src/components/Navbar.tsx
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className={"border px-6 py-4 flex justify-between items-center"}>
+    <nav
+      className={
+        "border px-6 py-4 flex justify-between items-center sticky top-0 z-50 bg-primary-foreground"
+      }
+    >
       <Link className="flex gap-2 items-center hover:bg-white" to="/">
         <FileTextIcon className="h-6 w-6" />
         <span className={"text-2xl font-semibold"}>PocketPortfolio</span>
